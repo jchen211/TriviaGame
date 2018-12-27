31ebde57c8087ef28c22c020e8fd39be89431550
Jessica Chen <jchen@jchen.local>
2018-12-26 21:03:27 -0600

    idea to approach js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,20 @@
 $(document).ready(funtion(){
 
+// start game-button -- on click function
 
+// once game starts. timer starts, question comes up.
+
+// hovering over the answer -- border shows
+
+// click on userChoice or timeOut
+    // wrong choice = shows correct choice
+    // right choice = correct screen
+
+// after 2 second. move on to next question
+
+// keeps going until all questions answered
+
+//end screen- shows # of correct and # of wrongs
+//reset game with a button.
 
 });
